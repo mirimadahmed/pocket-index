df20df762c9f4e6db6b9118685d7520e3e188108
Waleed Arshad Awan <waleedmalik677@gmail.com>
2022-01-15 22:13:19 +0500

    only mm

--- src/components/Account/config.js
+++ src/components/Account/config.js
@@ -12,41 +12,41 @@ export const connectors = [
     icon: Metamask,
     connectorId: "injected",
     priority: 1,
-  },
-  {
-    title: "WalletConnect",
-    icon: WalletConnect,
-    connectorId: "walletconnect",
-    priority: 2,
-  },
-  {
-    title: "Trust Wallet",
-    icon: TrustWallet,
-    connectorId: "injected",
-    priority: 3,
-  },
-  {
-    title: "MathWallet",
-    icon: MathWallet,
-    connectorId: "injected",
-    priority: 999,
-  },
-  {
-    title: "TokenPocket",
-    icon: TokenPocket,
-    connectorId: "injected",
-    priority: 999,
-  },
-  {
-    title: "SafePal",
-    icon: SafePal,
-    connectorId: "injected",
-    priority: 999,
-  },
-  {
-    title: "Coin98",
-    icon: Coin98,
-    connectorId: "injected",
-    priority: 999,
-  },
+  }
+  // {
+  //   title: "WalletConnect",
+  //   icon: WalletConnect,
+  //   connectorId: "walletconnect",
+  //   priority: 2,
+  // },
+  // {
+  //   title: "Trust Wallet",
+  //   icon: TrustWallet,
+  //   connectorId: "injected",
+  //   priority: 3,
+  // },
+  // {
+  //   title: "MathWallet",
+  //   icon: MathWallet,
+  //   connectorId: "injected",
+  //   priority: 999,
+  // },
+  // {
+  //   title: "TokenPocket",
+  //   icon: TokenPocket,
+  //   connectorId: "injected",
+  //   priority: 999,
+  // },
+  // {
+  //   title: "SafePal",
+  //   icon: SafePal,
+  //   connectorId: "injected",
+  //   priority: 999,
+  // },
+  // {
+  //   title: "Coin98",
+  //   icon: Coin98,
+  //   connectorId: "injected",
+  //   priority: 999,
+  // },
 ];
